7e665bfe2cfd8e527dac74288118b1ba75a567fa
Kevin Lu <jlu@splunk.com>
2020-05-28 12:36:23 -0700

    add dependencies

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -1,12 +1,12 @@
 class profile::nginx {
   package { 'nginx':
     ensure => installed,
-  }
+  } ->
 
   file { '/etc/nginx/conf.d/10-test.conf':
     ensure => 'file',
     source => 'puppet:///modules/profile/test.conf',
-  }
+  } ~>
 
   service { 'nginx':
     ensure => 'running',
