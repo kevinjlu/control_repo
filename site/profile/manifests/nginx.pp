049806f832e21a3134be74c707b966dfadba4dd3
Kevin Lu <jlu@splunk.com>
2020-05-28 12:57:06 -0700

    template port

--- site/profile/manifests/nginx.pp
+++ site/profile/manifests/nginx.pp
@@ -1,11 +1,12 @@
-class profile::nginx {
+class profile::nginx (String $port = '80') {
   package { 'nginx':
     ensure => installed,
   } ->
 
   file { '/etc/nginx/conf.d/10-test.conf':
     ensure => 'file',
-    source => 'puppet:///modules/profile/test.conf',
+    # source => 'puppet:///modules/profile/test.conf',
+    content => template('profile/test.conf.erb')
   } ~>
 
   service { 'nginx':
